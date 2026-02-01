412d697b6fb3e5aad5d89060dd4c8455d9df6d71
dima.yeromin <dima.yeromin@gmail.com>
2026-02-01 11:57:25 +0100

    changed docker settings

--- todolist/settings.py
+++ todolist/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': 'app_db',
         'USER': 'app_user',
         'PASSWORD': '1234',
-        'HOST': '172.17.0.2',  # You can use a different host if your MySQL server is on a remote machine.
+        'HOST': os.environ.get('DB_HOST', 'db'),  # Use service name from docker-compose
         'PORT': '',  # Leave this empty to use the default MySQL port (3306).
     }
 }
